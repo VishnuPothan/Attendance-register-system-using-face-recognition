d55ea25008c294c656fc381e6cc33527be8d38e8
Vishnu <vishnupothan2@gmail.com>
2021-04-08 08:28:01 +0530

    add class option added

--- app.py
+++ app.py
@@ -2,14 +2,19 @@
 from sklearn.preprocessing import LabelEncoder
 from env_variables import firebaseConfig
 from imutils.video import VideoStream
+from PIL import  Image as Img
+from PIL import ImageTk
 from imutils.video import FPS
 from sklearn.svm import SVC
 from tkinter import messagebox
 from imutils import paths
+from tkinter import Tk, Label
 import pyrebase
-from tkinter import*
+from tkinter import *
+import tkinter as tki
 import numpy as np
 import imutils
+import datetime
 import pickle
 import json
 import time
@@ -68,6 +73,12 @@ class HomeScreen():
         self.btnViewStudent = Button(self.f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=20, text="View Students", bg="powder blue",command=None)
         self.btnViewStudent.grid(row=16, column=1)
 
+        self.lblDash = Label(self.f1,text="---------------------",fg="white")
+        self.lblDash.grid(row=9,columnspan=3)
+
+        self.btnAddClass = Button(self.f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=20, text="Add Class", bg="powder blue",command = self.AddClassPressed)
+        self.btnAddClass.grid(row=10, column=1)
+
         self.root.mainloop()
 
     def AddTeacherPressed(self):
@@ -78,6 +89,9 @@ class HomeScreen():
     
     def StartAttendancePressed(self):
         self.startAttendance = StartAttendance()
+
+    def AddClassPressed(self):
+        self.addClass = AddClass()
 
 # ADD TEACHER SCREEN
 class AddTeacher():
@@ -562,9 +576,275 @@ class AddStudent():
         #ResetStudentWindow()
         self.rootAddStudent.destroy()
 
-# Start ATTENDANCE
+# START ATTENDANCE
 class StartAttendance():
     def __init__(self):
         os.system('cmd /k' "python recognize_video.py --detector model --embedding-model model/openface_nn4.small2.v1.t7 --recognizer output/face_recognizer.pickle --le output/face_label.pickle")
+                
+        #-----------------INITIALIZE TKINTER UI ADD STUDENT-------------------------------------------------------------------
+        """ self.rootAttendance = tki.Toplevel()
+        self.rootAttendance.title("Attendance Register Using Face Recognition")
+        self.width, self.height = self.rootAttendance.winfo_screenwidth(), self.rootAttendance.winfo_screenheight()
+        self.rootAttendance.geometry('%dx%d+0+0' % (self.width,self.height))
 
+        Label(self.rootAttendance,  font=( 'aria' ,30, 'bold' ),text="Attendance Register Using Face Recognition",fg="steel blue",bd=10,anchor='w').pack()
+        self.f1 = LabelFrame(self.rootAttendance,bg='red')
+
+        self.lblInfo = Label(self.rootAttendance, font=( 'aria' ,20, ),text="Attendance Record",fg="steel blue",anchor=W).pack()
+
+        self.f1.pack()
+        self.L1 = Label(self.f1, bg='powder blue')
+        self.L1.pack()
+        self.cap = VideoStream().start()
+
+        self.detectorPath = "model"
+        self.embeddingModelPath = "model/openface_nn4.small2.v1.t7"
+        self.recognizerPath = "output/face_recognizer.pickle"
+        self.labelPath = "output/face_label.pickle"
+
+        self.detectorPath = "model"
+        self.embeddingModelPath = "model/openface_nn4.small2.v1.t7"
+        self.recognizerPath = "output/face_recognizer.pickle"
+        self.labelPath = "output/face_label.pickle"
+
+        # load our serialized face detector from disk
+        print("[INFO] loading face detector...")
+        self.protoPath = os.path.sep.join([self.detectorPath, "deploy.prototxt"])
+        self.modelPath = os.path.sep.join([self.detectorPath, "res10_300x300_ssd_iter_140000.caffemodel"])
+        self.detector = cv2.dnn.readNetFromCaffe(self.protoPath, self.modelPath)
+
+        # load our serialized face embedding model from disk
+        print("[INFO] loading face recognizer...")
+        self.embedder = cv2.dnn.readNetFromTorch(self.embeddingModelPath)
+
+        # load the actual face recognition model along with the label encoder
+        self.recognizer = pickle.loads(open(self.recognizerPath, "rb").read())
+        self.le = pickle.loads(open(self.labelPath, "rb").read())
+
+        # teacher found
+        self.record_atendance = False
+        self.attendance_list = []
+        self.recorded = []
+        self.subject = ""
+
+        # Loope Video frames until stop condition
+        while True:
+            # Read Frame
+            self.frame = self.cap.read()
+            (self.h, self.w) = self.frame.shape[:2]
+            self.frame = imutils.resize(self.frame, width=300)
+            self.img = self.cap.read()
+
+            # Image conver to RGB
+            self.image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+            self.image = Img.fromarray(self.image)
+            self.image = ImageTk.PhotoImage(self.image)
+
+            # construct a blob from the image
+            self.imageBlob = cv2.dnn.blobFromImage(cv2.resize(self.frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+            # faces in the input image
+            self.detector.setInput(self.imageBlob)
+            self.detections = self.detector.forward()
+
+            # loop over the detections
+            for self.i in range(0, self.detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with
+                # the prediction
+                self.confidence = self.detections[0, 0, self.i, 2]
+
+                # filter out weak detections
+                if self.confidence > 0.5:
+                    # compute the (x, y)-coordinates of the bounding box for
+                    # the face
+                    self.box = self.detections[0, 0, self.i, 3:7] * np.array([self.w, self.h, self.w, self.h])
+                    (self.startX, self.startY, self.endX, self.endY) = self.box.astype("int")
+
+                    # extract the face ROI
+                    self.face = self.frame[self.startY:self.endY, self.startX:self.endX]
+                    (self.fH, self.fW) = self.face.shape[:2]
+
+                    # ensure the face width and height are sufficiently large
+                    if self.fW < 20 or self.fH < 20:
+                        continue
+
+                    # construct a blob for the face ROI, then pass the blob
+                    # through our face embedding model to obtain the 128-d
+                    # quantification of the face
+                    self.faceBlob = cv2.dnn.blobFromImage(self.face, 1.0 / 255, (96, 96), (0, 0, 0), swapRB=True, crop=False)
+                    self.embedder.setInput(self.faceBlob)
+                    self.vec = self.embedder.forward()
+
+                    # perform classification to recognize the face
+                    self.preds = self.recognizer.predict_proba(self.vec)[0]
+                    self.j = np.argmax(self.preds)
+                    self.proba = self.preds[self.j]
+                    self.name = self.le.classes_[self.j]
+
+                    # teacher face check
+                    with open('dataset/teacher_data.json') as self.teacherFile:
+                        self.data = json.load(self.teacherFile)
+                            
+                    if(self.record_atendance):
+                        # draw the bounding box
+                        self.text = "{}: {:.2f}%".format(self.name, self.proba * 100)
+                        self.y = self.startY - 10 if self.startY - 10 > 10 else self.startY + 10
+                        cv2.rectangle(self.frame, (self.startX, self.startY), (self.endX, self.endY), (0, 0, 255), 2)
+                        cv2.putText(self.frame, self.text, (self.startX, self.y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+
+                        # record attendance to file
+                        with open('dataset/student_data.json') as self.studentFile:
+                            self.data = json.load(self.studentFile)
+                        for self.studentDict in self.data["student"]:
+                            if(self.studentDict["ID"] == self.name):
+                                if(self.name not in self.recorded):
+                                    self.now = datetime.datetime.now()
+                                    self.current_time = self.now.strftime("%H:%M:%S")
+                                    self.student_dict = {"ID" : self.name, "name" : self.studentDict["name"], "time" : str(self.current_time), "subject" : self.subject}
+                                    self.attendance_list.append(self.student_dict)
+                                    self.recorded.append(self.name)
+                    else:
+                        # check if teacher
+                        for self.teacherDict in self.data["teacher"]:
+
+                            if(self.teacherDict["ID"] == self.name):
+
+                                # draw the bounding box
+                                self.text = "{}: {:.2f}%".format(self.name, self.proba * 100)
+                                self.y = self.startY - 10 if self.startY - 10 > 10 else self.startY + 10
+                                cv2.rectangle(self.frame, (self.startX, self.startY), (self.endX, self.endY), (0, 0, 255), 2)
+                                cv2.putText(self.frame, self.text, (self.startX, self.y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+
+                                print(self.teacherDict)
+
+                                self.verify = input("Record attendance(yes/no) : ")
+
+                                if(self.verify == "yes"):
+                                    self.record_atendance = True
+                                    self.subject = self.teacherDict["subject"]
+
+            print(self.attendance_list);
+
+            # Update tikter screen
+            if self.L1 is None:
+                self.L1 = tki.Label(self.f1, bg='steel blue', image = self.image)
+                self.L1.image = self.image
+                self.L1.pack()
+            else:
+                self.L1.configure(image=self.image)
+                self.L1.image = self.img
+            self.rootAttendance.update()
+
+            # show the output frame
+            cv2.imshow("Frame", self.frame)
+
+            self.key = cv2.waitKey(1) & 0xFF
+
+            # if the `s` key was pressed, stop attendance recording
+            if self.key == ord("s"):
+                print(self.attendance_list)
+                attendance_dict = {self.subject : self.attendance_list}
+                #db.child("attendance").child(str(datetime.date.today())).push(attendance_dict)
+                self.record_atendance = False
+                self.attendance_list = []
+                self.recorded = []
+                self.subject = ""
+                
+            # if the `q` key was pressed, break from the loop
+            if self.key == ord("q"):
+                break
+
+        print(self.attendance_list)
+            
+        # do a bit of cleanup
+        self.cap.stream.release()
+        cv2.destroyAllWindows() """
+
+# ADD CLASS
+class AddClass():
+    def __init__(self):
+        #-----------------INITIALIZE TKINTER UI ADD TEACHER-------------------------------------------------------------------
+        self.rootAddClass = Tk() 
+        self.rootAddClass.title("Attendance Register Using Face Recognition")
+        self.width, self.height = self.rootAddClass.winfo_screenwidth(), self.rootAddClass.winfo_screenheight()
+        self.rootAddClass.geometry('%dx%d+0+0' % (self.width,self.height))
+        self.rootAddClass.lift()
+
+        # variables
+        self.nameClass = StringVar(self.rootAddClass)
+        self.lateTime = StringVar(self.rootAddClass)
+        self.hoursPerDay = StringVar(self.rootAddClass)
+
+        #-----------------INFO TOP-----------------------------------------------------------------------------
+        self.Tops = Frame(self.rootAddClass,bg="white",width = 1600,height=50,relief=SUNKEN)
+        self.Tops.pack(side=TOP)
+
+        self.lblInfo = Label(self.Tops, font=( 'aria' ,30, 'bold' ),text="Attendance Register Using Face Recognition",fg="steel blue",bd=10,anchor='w')
+        self.lblInfo.grid(row=0,column=0)
+        self.lblInfo = Label(self.Tops, font=( 'aria' ,20, ),text="Add Teacher",fg="steel blue",anchor=W)
+        self.lblInfo.grid(row=1,column=0)
+
+        #-----------------FORM ITEMS--------------------------------------------------------------------------------------------
+        self.f1 = Frame(self.rootAddClass,width = 900,height=700,relief=SUNKEN)
+        self.f1.pack(side=TOP)
+
+        # Name input
+        self.lblDash = Label(self.f1,text="---------------------",fg="white")
+        self.lblDash.grid(row=0,columnspan=3)
+
+        self.lblName = Label(self.f1, font=( 'aria' ,16, 'bold' ),text="Class Name",fg="steel blue",bd=10,anchor='w')
+        self.lblName.grid(row=1,column=0)
+
+        self.txtName = Entry(self.f1,font=('ariel' ,16,'bold'), textvariable = self.nameClass , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+        self.txtName.grid(row=1,column=1)
+
+        # Late input    
+        self.lblDash = Label(self.f1,text="---------------------",fg="white")
+        self.lblDash.grid(row=2,columnspan=3)
+
+        self.lblLateTime = Label(self.f1, font=( 'aria' ,16, 'bold' ),text="Late Time",fg="steel blue",bd=10,anchor='w')
+        self.lblLateTime.grid(row=3,column=0)
+
+        self.txtLateTime = Entry(self.f1,font=('ariel' ,16,'bold'), textvariable = self.lateTime , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+        self.txtLateTime.grid(row=3,column=1)
+
+        # Hours input
+        self.lblDash = Label(self.f1,text="---------------------",fg="white")
+        self.lblDash.grid(row=4,columnspan=3)
+
+        self.lblHoursPerDat = Label(self.f1, font=( 'aria' ,16, 'bold' ),text="Hours Per Day",fg="steel blue",bd=10,anchor='w')
+        self.lblHoursPerDat.grid(row=5,column=0)
+
+        self.txtHoursPerDat = Entry(self.f1,font=('ariel' ,16,'bold'), textvariable = self.hoursPerDay , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+        self.txtHoursPerDat.grid(row=5,column=1)
+
+        # Add Button
+        self.lblDash = Label(self.f1,text="---------------------",fg="white")
+        self.lblDash.grid(row=8,columnspan=3)
+
+        self.btnStart = Button(self.f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=20, text="Add", bg="powder blue",command = self.AddNewClass)
+        self.btnStart.grid(row=9, column=1)
+
+        self.rootAddClass.mainloop()
+
+    def AddNewClass(self):
+        if (len(self.nameClass.get()) == 0 or len(self.lateTime.get()) == 0 or len(self.hoursPerDay.get()) == 0):
+            messagebox.showerror("Error", "Enter Class Details To Continue")
+            return
+        
+        # write data to firebase
+        # initlaize firebase connection
+        self.firebase = pyrebase.initialize_app(firebaseConfig)
+        self.db = self.firebase.database()
+
+        # write to realtime db firebase
+        self.classDict = {"name" : self.nameClass.get(), "late" : self.lateTime.get(), 'hours' : self.hoursPerDay.get()}
+        self.db.child("class").child(self.nameClass.get()).set(self.classDict)
+
+        # Reset Window
+        self.rootAddClass.destroy()
+
+# CALL MAIN
 homeScreen = HomeScreen()
